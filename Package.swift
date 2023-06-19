99aaacedb0f6ad3995d68e9b3afa817b0eb3a342
Esenbek <esenbekkd@gmail.com>
2023-06-19 17:14:46 +0600

    Remove HCaptcha_RxSwift from SPM package

--- Package.swift
+++ Package.swift
@@ -13,31 +13,15 @@ let package = Package(
         .library(
             name: "HCaptcha",
             targets: ["HCaptcha"]
-        ),
-        .library(
-            name: "HCaptcha_RxSwift",
-            targets: ["HCaptcha_RxSwift"]
         )
-    ],
-    dependencies: [
-        .package(url: "https://github.com/ReactiveX/RxSwift", .upToNextMajor(from: "6.2.0"))
     ],
     targets: [
         .target(
             name: "HCaptcha",
             path: "HCaptcha",
-            exclude: ["Classes/Rx"],
             resources: [
                 .process("Assets/hcaptcha.html")
             ]
-        ),
-        .target(
-            name: "HCaptcha_RxSwift",
-            dependencies: [
-                "HCaptcha",
-                .product(name: "RxSwift", package: "RxSwift")
-            ],
-            path: "HCaptcha/Classes/Rx"
         )
     ]
 )
